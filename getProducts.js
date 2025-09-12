ac483d8110e37257c5884ceef778575403b49df5
Eliah Dimmed <153503499+eliahdim@users.noreply.github.com>
2025-09-12 09:47:00 +0200

    added images to all products and made random products appear

--- getProducts.js
+++ getProducts.js
@@ -1,4 +1,12 @@
 // Fetch products from the JSON file
+let product1 = Math.floor(Math.random()*6);
+let product2 = Math.floor(Math.random()*6);
+let product3 = Math.floor(Math.random()*6);
+
+
+
+console.log(product1);
+
 fetch('products.json')
   .then(response => response.json())
   .then(products => {
@@ -6,9 +14,9 @@ fetch('products.json')
 
     // Choose the 3 products you want to display
     const selectedProducts = [
-      products[0], // Zoegas
-      products[1], // Löfbergs
-      products[2]  // Illy
+      products[product1], // Zoegas
+      products[product2], // Löfbergs
+      products[product3]  // Illy
     ];
 
     selectedProducts.forEach(product => {
@@ -17,7 +25,7 @@ fetch('products.json')
       card.className = 'col';
       card.innerHTML = `
         <div class="card">
-          <img src="${product.image}" class="card-img-top" alt="${product.Brand} ${product.Type}">
+          <img src="${product.image}" class="card-img-top pt-4" alt="${product.Brand} ${product.Type}">
           <div class="card-body">
             <h4 class="card-title">${product.Brand}</h4>
             <h5 class="card-title">${product.Type}</h5>
@@ -25,7 +33,7 @@ fetch('products.json')
               Price: ${product.Price.toFixed(2)}:-<br>
               Weight: ${product.Weight}<br>
             </p>
-            <a href="#" class="btn btn-primary" addToCart()>Buy Now</a>
+            <button type="button" class="btn btn-primary" onclick="addToCart()">Buy Now</button>
           </div>
         </div>
       `;
@@ -35,3 +43,8 @@ fetch('products.json')
   .catch(error => console.error('Error fetching products:', error));
 
   console.log("Products loaded successfully");
+
+  function addToCart() {
+    document.cookie = "product="
+    alert("Product added to cart!");
+  }
